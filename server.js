ba8e615f4dc38b0fd75c7f446605797809f8e39b
Maddy <mmcintosh2k14@gmail.com>
2019-12-04 17:37:49 -0500

    debugs code to get a list of users

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const userRouter = require('./users/userRouter');
+
 const server = express();
 
 server.get('/', (req, res) => {
@@ -15,54 +17,11 @@ function logger(req, res, next) {
   next();
 };
 
-// to be used on every request that expects a user id parameter.
-function validateUserId(req, res, next) {
-  const id = req.params.id;
-  const user = req.user;
-
-  if (id.length > 0) {
-    user
-    next();
-  } else {
-    res
-      .status(400)
-      .json({ message: 'invalid user id.' });
-  }
-};
-
-// validates the body on a request to create a new user (POST)
-function validateUser(req, res, next) {
-  const userData = req.body;
-
-  if (userData.length === 0) {
-    res.status(400).json({ message: 'missing user data.' });
-  }
-  if (!userData.name) {
-    res.status(400).json({ message: 'missing required name field.' });
-  } else {
-    next();
-  }
-};
-
-// validates body on a request to create a new post 
-function validatePost(req, res, next) {
-  const postData = req.body;
-
-  if (postData.length === 0) {
-    res.status(400).json({ message: 'missing post data.' });
-  }
-  if (!postData.text) {
-    res.status(400).json({ message: 'missing required text field.' })
-  } else {
-    next();
-  }
-};
-
 server.use(helmet());
 server.use(express.json());
-server.use(logger); // this will run globally (on every endpoint).
+server.use(logger);
 
-// endpoints
+server.use('/api/users', userRouter);
 
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const Users = require('./userDb');
+const Posts = require('../posts/postDb');
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -10,8 +13,16 @@ router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+// retrieves list of Users
 router.get('/', (req, res) => {
-  // do your magic!
+  Users.get(req.query)
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(err => {
+    console.log('Trouble retrieving users.', err);
+    res.status(500).json({ message: 'Error retrieving users.' });
+  });
 });
 
 router.get('/:id', (req, res) => {
@@ -31,17 +42,48 @@ router.put('/:id', (req, res) => {
 });
 
 //custom middleware
-
+//to be used on every request that expects a user id parameter.
 function validateUserId(req, res, next) {
-  // do your magic!
-}
+  const id = req.params.id;
+  const user = req.user;
 
+  if (id.length > 0) {
+    user
+  } else {
+    res
+      .status(400)
+      .json({ message: 'invalid user id.' });
+  }
+  next();
+};
+
+// // validates the body on a request to create a new user (POST)
 function validateUser(req, res, next) {
-  // do your magic!
-}
+  const userData = req.body;
 
+  if (userData.length === 0) {
+    res.status(400).json({ message: 'missing user data.' });
+  }
+  if (!userData.name) {
+    res.status(400).json({ message: 'missing required name field.' });
+  } else {
+    next();
+  }
+};
+
+// validates body on a request to create a new post 
 function validatePost(req, res, next) {
-  // do your magic!
-}
+  const postData = req.body;
+
+  if (postData.length === 0) {
+    res.status(400).json({ message: 'missing post data.' });
+  }
+  if (!postData.text) {
+    res.status(400).json({ message: 'missing required text field.' })
+  } else {
+    next();
+  }
+};
+
 
 module.exports = router;
